880bb27219df4853e7b14b2518ed7a2f361612e0
EphraimShikanga <ephraimshikanga@gmail.com>
2024-10-10 10:40:00 +0300

    feat: Enable workspace deletion
    This commit introduces the ability for users to delete workspaces.
    A trash icon is added to each workspace, allowing users to remove unwanted workspaces and manage their workflow effectively.

--- src/components/workspace_tab.tsx
+++ src/components/workspace_tab.tsx
@@ -1,5 +1,6 @@
 import {
     Button,
+    IconButton,
     Input,
     List,
     ListItem,
@@ -9,7 +10,7 @@ import {
     Typography
 } from "@material-tailwind/react";
 import React, {useState} from "react";
-import {Layers, Settings2} from "lucide-react";
+import {Layers} from "lucide-react";
 import {useWorkspace} from "@/lib/WorkContext.tsx";
 
 type Workspace = {
@@ -27,10 +28,27 @@ type WorkspaceTabProps = {
     value: string;
 };
 
+function TrashIcon() {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="h-5 w-5"
+        >
+            <path
+                fillRule="evenodd"
+                d="M16.5 4.478v.227a48.816 48.816 0 013.878.512.75.75 0 11-.256 1.478l-.209-.035-1.005 13.07a3 3 0 01-2.991 2.77H8.084a3 3 0 01-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 01-.256-1.478A48.567 48.567 0 017.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 013.369 0c1.603.051 2.815 1.387 2.815 2.951zm-6.136-1.452a51.196 51.196 0 013.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 00-6 0v-.113c0-.794.609-1.428 1.364-1.452zm-.355 5.945a.75.75 0 10-1.5.058l.347 9a.75.75 0 101.499-.058l-.346-9zm5.48.058a.75.75 0 10-1.498-.058l-.347 9a.75.75 0 001.5.058l.345-9z"
+                clipRule="evenodd"
+            />
+        </svg>
+    );
+}
+
 const Workspace: React.FC<WorkspaceProps> = ({workspace}) => {
     // const {selected} = useWorkspace();
     const [currentWorkspaceTabs, setCurrentWorkspaceTabs] = useState(0);
-    const {workspaces} = useWorkspace();
+    const {workspaces, setWorkspaces} = useWorkspace();
     // const getCurrentWorkspaceTabs = async () => {
     chrome.tabs.query({windowType: "normal"}, (tabs) => {
         setCurrentWorkspaceTabs(tabs.length);
@@ -47,67 +65,82 @@ const Workspace: React.FC<WorkspaceProps> = ({workspace}) => {
     return (
         // eslint-disable-next-line @typescript-eslint/ban-ts-comment
         // @ts-expect-error
-        <ListItem onClick={() => {
-            console.log(workspace.id);
-
-            chrome.tabs.query({windowType: "normal"}, (tabs) => {
-                const updatedWorkspaces = workspaces.map(w =>
-                    workspace.id === w.id ? {...w, isCurrent: true} : {...w, tabs: tabs, isCurrent: false}
-                );
-                // Perform tab-related operations
-                // chrome.runtime.sendMessage({ action: "createNewTabAndRemoveCurrent", currentTabs });
-                chrome.storage.local.set({'workspaces': updatedWorkspaces}, async () => {
-                    console.log("Workspaces saved successfully");
-                    if (workspace.tabs.length > 0) {
-                        workspace.tabs.forEach((tab) => {
-                            chrome.tabs.create({url: tab.url}).then(() => {
-                                console.log('Tab created successfully');
-                            }).catch((error) => {
-                                console.error('Error creating tab:', error);
-                            });
-                        });
-                        tabs.forEach((tab) => chrome.tabs.remove(tab.id!));
-                        await new Promise((resolve) => setTimeout(resolve, 5000));
-                        workspace.tabs.forEach((tab) => chrome.tabs.discard(tab.id!));
-                    } else {
-                        await chrome.tabs.create({url: "chrome://newtab", active: true});
-                        tabs.forEach((tab) => chrome.tabs.remove(tab.id!));
-                    }
-                    // setSelectedItem(workspace.id);
-                });
-            });
-
-
-        }} className={`bg-white/20 p-1 ${workspace.isCurrent ? "bg-white/70" : ""}`}
+        <ListItem className={`bg-white/20 p-1 ${workspace.isCurrent ? "bg-white/70" : ""}`}
         >
-            {/*eslint-disable-next-line @typescript-eslint/ban-ts-comment*/}
-            {/*@ts-expect-error*/}
-            <ListItemPrefix>
-                <Layers size={"28px"} color={"#1e293b"}/>
-            </ListItemPrefix>
-            <div>
+            <div className={"flex flex-row flex-grow"} onClick={() => {
+                chrome.tabs.query({windowType: "normal"}, (tabs) => {
+                    const updatedWorkspaces = workspaces.map(w =>
+                        workspace.id === w.id ? {...w, isCurrent: true} : {...w, tabs: tabs, isCurrent: false}
+                    );
+                    // Perform tab-related operations
+                    // chrome.runtime.sendMessage({ action: "createNewTabAndRemoveCurrent", currentTabs });
+                    chrome.storage.local.set({'workspaces': updatedWorkspaces}, async () => {
+                        console.log("Workspaces saved successfully");
+                        if (workspace.tabs.length > 0) {
+                            workspace.tabs.forEach((tab) => {
+                                chrome.tabs.create({url: tab.url}).then(() => {
+                                    console.log('Tab created successfully');
+                                }).catch((error) => {
+                                    console.error('Error creating tab:', error);
+                                });
+                            });
+                            tabs.forEach((tab) => chrome.tabs.remove(tab.id!));
+                            await new Promise((resolve) => setTimeout(resolve, 5000));
+                            workspace.tabs.forEach((tab) => chrome.tabs.discard(tab.id!));
+                        } else {
+                            await chrome.tabs.create({url: "chrome://newtab", active: true});
+                            tabs.forEach((tab) => chrome.tabs.remove(tab.id!));
+                        }
+                        // setSelectedItem(workspace.id);
+                    });
+                });
+            }}>
                 {/*eslint-disable-next-line @typescript-eslint/ban-ts-comment*/}
                 {/*@ts-expect-error*/}
-                <Typography variant="h6" color="blue-gray">
-                    {workspace.title}
-                </Typography>
-                {/*eslint-disable-next-line @typescript-eslint/ban-ts-comment*/}
-                {/*@ts-expect-error*/}
-                <Typography variant="small" color="blue-gray"
-                            className="font-normal">
-                    {
-                        workspace.isCurrent ?
-                            currentWorkspaceTabs
-                            :
-                            workspace.tabs.length
-                    } tabs
-                </Typography>
+                <ListItemPrefix>
+                    <Layers size={"28px"} color={"#1e293b"}/>
+                </ListItemPrefix>
+                <div className={" flex flex-col"}>
+                    {/*eslint-disable-next-line @typescript-eslint/ban-ts-comment*/}
+                    {/*@ts-expect-error*/}
+                    <Typography variant="h6" color="blue-gray">
+                        {workspace.title}
+                    </Typography>
+                    {/*eslint-disable-next-line @typescript-eslint/ban-ts-comment*/}
+                    {/*@ts-expect-error*/}
+                    <Typography variant="small" color="blue-gray"
+                                className="font-normal">
+                        {
+                            workspace.isCurrent ?
+                                currentWorkspaceTabs
+                                :
+                                workspace.tabs.length
+                        } tabs
+                    </Typography>
+                </div>
             </div>
-            {/*eslint-disable-next-line @typescript-eslint/ban-ts-comment*/}
-            {/*@ts-expect-error*/}
-            <ListItemSuffix>
-                <Settings2 size={"20px"} color={"black"}/>
-            </ListItemSuffix>
+
+            {
+                workspace.id !== 1 && (
+                    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                    // @ts-expect-error
+                    <ListItemSuffix>
+                        {/*eslint-disable-next-line @typescript-eslint/ban-ts-comment*/}
+                        {/*@ts-expect-error*/}
+                        <IconButton size={"24px"} variant="text" color="blue-gray" onClick={
+                            () => {
+                                const updatedWorkspaces = workspaces.filter(w => w.id !== workspace.id);
+                                chrome.storage.local.set({'workspaces': updatedWorkspaces}, () => {
+                                    setWorkspaces(updatedWorkspaces);
+                                    console.log("Workspace deleted successfully");
+                                });
+                            }
+                        }>
+                            <TrashIcon/>
+                        </IconButton>
+                    </ListItemSuffix>
+                )
+            }
         </ListItem>
     );
 
