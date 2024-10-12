28112e7047056335cd864411bd39e6a10e3c60b8
EphraimShikanga <ephraimshikanga@gmail.com>
2024-10-12 15:22:54 +0300

    feat: Integrate CRXJS Vite plugin for Chrome Extension development
    This integrates the CRXJS Vite plugin into the project, simplifying the development process for the Chrome Extension.
    
    The plugin streamlines the handling of the `manifest.json` file and bundles the extension correctly, replacing the previous custom Rollup configuration. This results in a cleaner and more maintainable development setup.

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,30 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
+// import { resolve } from 'path';
 import path from "path"
+import manifest from './public/manifest.json'
+import { crx } from '@crxjs/vite-plugin'
 
 export default defineConfig({
   build: {
     chunkSizeWarningLimit: 1500,
-    rollupOptions: {
-      input: {
-        popup: resolve(__dirname, 'index.html'), // Include popup.html or index.html for the default popup
-        background: resolve(__dirname, 'src/background/background2.ts'), // Background script
-      },
-      output: {
-        entryFileNames: '[name].js', // Ensures consistent file names
-      }
-    }
+    // rollupOptions: {
+    //   input: {
+    //     popup: resolve(__dirname, 'index.html'), // Include popup.html or index.html for the default popup
+    //     background: resolve(__dirname, 'src/background/background2.ts'), // Background script
+    //   },
+    //   output: {
+    //     entryFileNames: '[name].js', // Ensures consistent file names
+    //   }
+    // }
   },
-  plugins: [react()],
+  plugins: [crx({ manifest }),react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  server: {
+    port: 5173,
   },
 });
